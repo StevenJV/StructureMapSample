97238491398b46dcdf9c6a8ad6aec97219540023
StevenJV <steven@vorefamily.net>
2014-07-31 15:27:40 -0400

    sample code for simple use of StructureMap

---
+++ StructureMapSample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace StructureMapSample
+{
+  public class Program
+  {
+    static void Main(string[] args) {
+      ObjectFactory.Configure(config => {
+        config.Scan(scan => {
+          scan.TheCallingAssembly();
+          scan.WithDefaultConventions();
+        });
+        config.For<IDumpable>().Use<DumpToScreen>();
+      });
+
+      var steven = new Person { Name = "Steven" };
+      var jace = new Person { Name = "Jace" };
+
+      var dumper = ObjectFactory.GetInstance<IDumpable>();
+
+      dumper.Dump(steven);
+    }
+  }
+
+  class Person
+  {
+    public string Name;
+  }
+
+
+
+  interface IDumpable
+  {
+    void Dump(Person p);
+  }
+
+  class DumpToScreen : IDumpable
+  {
+    public void Dump(Person p) {
+      Console.WriteLine(p.Name);
+      Console.ReadKey();
+    }
+  }
+
+  class DumpToPrinter : IDumpable
+  {
+    public void Dump(Person p) {
+      Console.WriteLine(p.Name);
+    }
+
+  }
+
+}
