4fcd0c101d9ef72cc36d676e44c20e7977193123
StevenJV <steven@vorefamily.net>
2014-07-31 15:48:11 -0400

    slightly more complex, using adaptor pattern

--- StructureMapSample/Program.cs
+++ StructureMapSample/Program.cs
@@ -13,14 +13,15 @@ namespace StructureMapSample
           scan.WithDefaultConventions();
         });
         config.For<IDumpable>().Use<DumpToScreen>();
+        config.For<IDumpPeople>().Use<DumpPeople>();
       });
 
       var steven = new Person { Name = "Steven" };
       var jace = new Person { Name = "Jace" };
 
-      var dumper = ObjectFactory.GetInstance<IDumpable>();
+      var dumper = ObjectFactory.GetInstance<IDumpPeople>();
 
-      dumper.Dump(steven);
+      dumper.DumpPerson(steven);
     }
   }
 
@@ -29,25 +30,51 @@ namespace StructureMapSample
     public string Name;
   }
 
+  static class PersonExtensions
+  {
+    public static IDumpable DefaultDumper = ObjectFactory.GetInstance<IDumpable>();
 
+    public static void Dump(this Person p) {
+      DefaultDumper.Dump(p);
+    }
+  }
+
+  //adapter
+  interface IDumpPeople
+  {
+    void DumpPerson(Person p);
+  }
+
+  class DumpPeople : IDumpPeople
+  {
+    private readonly IDumpable _dumper;
+
+    public DumpPeople(IDumpable dumper) {
+      _dumper = dumper;
+    }
+
+    public void DumpPerson(Person p) {
+      _dumper.Dump(p.Name);
+    }
+  }
 
   interface IDumpable
   {
-    void Dump(Person p);
+    void Dump(Object p);
   }
 
   class DumpToScreen : IDumpable
   {
-    public void Dump(Person p) {
-      Console.WriteLine(p.Name);
+    public void Dump(object o) {
+      Console.WriteLine(o);
       Console.ReadKey();
     }
   }
 
   class DumpToPrinter : IDumpable
   {
-    public void Dump(Person p) {
-      Console.WriteLine(p.Name);
+    public void Dump(object o) {
+      Console.WriteLine(o);
     }
 
   }
